01815abb073061ea7cb53d6ba441a5602557f4f5
littlesekii <contact.sekii@gmail.com>
2025-03-31 23:25:25 -0300

    project started

---
+++ src/views/MainView.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <p>Hello, Linky Cat!</p>
+  </main>
+  
+</template>
+
+
+<style scoped>
+main {
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+main p {
+  font-size: 18pt;
+}
+</style>
