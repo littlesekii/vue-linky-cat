10211fc55cae28d46d40625fe2f93f241adfda8e
littlesekii <contact.sekii@gmail.com>
2025-04-01 17:58:13 -0300

    feat.: created LinkButton component

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const data = defineProps([
+  "title",
+  "url"
+]);
+
+function goto() {
+  window.open(data.url);
+}
+
+</script>
+
+<template>
+  <div @click="goto" class="link-button">
+    <p>{{ data.title }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+.link-button {
+  padding: 1em;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 0.1px solid white;
+  cursor: pointer;
+
+  user-select: none;
+
+  border-radius: 5px;
+}
+
+</style>
--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -1,14 +1,17 @@
 <script setup>
-
+import LinkButton from "@/components/LinkButton.vue";
 </script>
 
 <template>
   <div>
-    
+    <LinkButton title="Twitch" url="https://twitch.tv/littlesekii" />
+    <LinkButton title="Youtube" url="https://youtube.com/@littlesekii" />
   </div>
 </template>
 
 
 <style scoped>
-
+  div {
+    padding: 10px;
+  }
 </style>
